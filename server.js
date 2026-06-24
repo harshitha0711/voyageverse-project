4e647b273b9be70f6c271d4448738b831613cbbc
harshitha0711 <24je0711@iitism.ac.in>
2026-06-24 08:42:04 +0530

    fix: add frontend URL to CORS allowedOrigins

--- server.js
+++ server.js
@@ -31,7 +31,8 @@ app.use((req, res, next) => {
     'http://127.0.0.1:5500', 
     'http://localhost:5500',
     'http://localhost:5173',
-    'http://127.0.0.1:5173'
+    'http://127.0.0.1:5173',
+    'https://voyageverse-frontend.onrender.com'
   ];
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
